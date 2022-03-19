b4152bb260a09f23899a1501726a98b3dbfb13fc
azuki774 <naruki.d@gmail.com>
2022-03-20 00:52:37 +0900

    add csv-import test1

--- internal/csv-import/file_test.go
+++ internal/csv-import/file_test.go
@@ -7,6 +7,13 @@ import (
 )
 
 func Test_portCSVToString(t *testing.T) {
+	dir, _ := os.Getwd()
+	testfile1, _ := os.Open(dir + "/test/1999-01-23.csv")
+	rowData10 := []string{"取引", "ファンド名", "買付日", "数量", "取得単価", "現在値", "前日比", "前日比（％）", "損益", "損益（％）", "評価額", "編集"}
+	rowData11 := []string{"積立  売却", "AAA", "--/--/--", "26231", "13000", "11403", "-258", "-2.21", "-4189.09", "-12.28", "29911.2", "詳細 "}
+	rowData12 := []string{"積立  売却", "BBB", "--/--/--", "10946", "31610", "29726", "+235", "+0.80", "-2062.22", "-5.96", "32538.07", "詳細 "}
+	collect1 := [][]string{rowData10, rowData11, rowData12}
+
 	type args struct {
 		osf *os.File
 	}
@@ -16,7 +23,12 @@ func Test_portCSVToString(t *testing.T) {
 		wantRecords [][]string
 		wantErr     bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:        "test/1999-01-23.csv",
+			args:        args{osf: testfile1},
+			wantRecords: collect1,
+			wantErr:     false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
